7476cb49b6b0a98284aaca1be01d8cce1d680f5b
Sihyeon0123 <129951793+Sihyeon0123@users.noreply.github.com>
2024-05-22 00:25:09 +0900

    메서드 이름 변경

--- Python/16719-ZOAC.py
+++ Python/16719-ZOAC.py
@@ -1,7 +1,10 @@
+# ZOAC (https://www.acmicpc.net/problem/16719)
+# 재귀함수
+
 import sys
 input =sys.stdin.readline  
 
-arr = list(input().rstrip()) # 문자열을 하나의 리스트로 받는것.(list)
+arr = list(input().rstrip())
 
 result = [""]*len(arr)
 
@@ -14,9 +17,6 @@ def solution(start,arr):
     result[start + temp] = min_val
     print("".join(result))
     solution(start+temp+1,arr[temp+1:]) 
-    #가장 작은 거 기준으로 오른쪽으로 먼저 재귀해야 가장 사전순으로 작은 값이됨
-    #ex) A x->  CA  // A -> AQ  // 이와 같이 가장작은거 기준 왼쪽으로 먼저 값을넣게되면
-    #사전순서를 만족 시킬수 없다.
     solution(start,arr[:temp])
 
 solution(0,arr)
